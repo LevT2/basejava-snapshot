41fc8ed2c7148d050a70ca3cdf20cda2c423fb28
Lev Tikhonov <levt2@mail.ua>
2019-09-15 20:39:25 +0300

    MainRecursion

---
+++ src/ru/javawebinar/basejava/MainRecursion.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.basejava;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.System.getProperty;
+
+public class MainRecursion {
+    protected static final String ROOT = System.getProperty("user.dir");
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(getProperty("user.dir"), "src");
+        prettyPrint(path);
+    }
+
+    private static void prettyPrint(Path path) throws IOException {
+        Files.walk(path).
+                filter(Files::isDirectory).
+                forEach(childpath -> {
+                    System.out.println(childpath);
+                    try (var files = Files.newDirectoryStream(childpath, Files::isRegularFile)) {
+                        files.forEach(file -> {
+                            System.out.print(" ".repeat(file.getParent().toString().length()));
+                            System.out.println(file.getFileName().toString());
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
